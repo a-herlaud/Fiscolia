1449df97389bf8e27f4817545cf55a4dcdc3af67
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2026-05-11 12:41:34 +0200

    feat(init.sh): make a script that initialize the vault
    - to give the address and the id token of the vault to the container
    - to create key-value with project's secrets from .env and /secrets

---
+++ srcs/hashi-corp-vault/init.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -e
+
+vault server -dev \
+    -dev-listen-address=${VAULT_DEV_LISTEN_ADDRESS} \
+    -dev-root-token-id="${VAULT_DEV_ROOT_TOKEN_ID}" &
+
+sleep 3
+export VAULT_ADDR=${VAULT_ADDR}
+export VAULT_TOKEN=${VAULT_DEV_ROOT_TOKEN_ID}
+
+vault status
+vault secrets enable -version=1 -path=legacy-secrets kv
+
+vault kv put secret/database \
+    project_name=${PROJECT_NAME} \
+    db_user=${DB_AUTH_USER} \
+    db_password=@/run/secrets/db_auth_pwd \
+    db_port=${DB_AUTH_PORT} \
+    db_name=${DB_AUTH_NAME} \
+    postres_password=@/run/secrets/db_root_pwd \
+    backend_port=${BACK_AUTH_PORT}  \
+    upload_port=${BACK_UPLOAD_PORT} \
+    front_port=${FRONT_PORT}    \
+    proxy_port=${PROXY_EXPOSED_PORT}    \
+
+
+
+
+#vault kv put -mount=secret PROJECT_NAME=${PROJECT_NAME}
+
+wait
