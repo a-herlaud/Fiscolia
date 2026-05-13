3f32471706ed28a27570ade4c65b3c8eb5555426
aherlaud <aherlaud@student.42.fr>
2026-05-13 20:16:56 +0200

    fix(AI-chatbot): fix multiple issues around the chatbot branch
    - secrets/*: create the secrets repo with examples to run the project easily
    - srcs/docker-compose.yml: remove profiles, use cpu to run ollama models and put back the dependencies to the chatbot
    - srcs/frontend/srcs/pages/Homepage/Homepage.jsx: remove the backend button access not handled anymore
    - srcs/proxy/.conf: change the resolver to the podman one to run the project at school

--- srcs/frontend/srcs/pages/Homepage/Homepage.jsx
+++ srcs/frontend/srcs/pages/Homepage/Homepage.jsx
@@ -7,12 +7,6 @@ function Home() {
 	<div>
 		<p>Un projet, une vision</p>
 		<h1 style={{ color: '#000091', fontFamily: 'montserat'}}>Fiscolia</h1>
-	
-	<div>
-		<Link to="/backend">
-			<button>BACKEND</button>
-		</Link>
-	</div>
 	<div>
 		<Link to="/login">
 			<button>LOGIN</button>
