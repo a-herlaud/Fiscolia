e9c15f00edfc4d6a0ce20e0a4d948a7381dea4b0
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2026-06-03 16:55:29 +0200

    feat(modify-profil): update style

--- srcs/frontend/srcs/pages/EditProfile/EditProfile.jsx
+++ srcs/frontend/srcs/pages/EditProfile/EditProfile.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import style from '../Register/Register.module.css';
+import '../../index.css'
+import './EditProfile.css'
 
 function EditProfile() {
   const [message, setMessage] = useState("");
@@ -43,27 +45,27 @@ function EditProfile() {
   return (
 
 
-	<div className={style.pages}>
-	  <form className={style.form} onSubmit={handleSubmit}>
-	  <h1 className={style.page_title}>Je change mon profil</h1>
-	  <div className={style.field_container}>
-		<p className={style.field_name}>Etat civil</p>
+	<div className="center-body-style">
+	  <form className="modify_form" onSubmit={handleSubmit}>
+	  <h1>Je change mon profil</h1>
+	  <div className="auth-field-container">
+		<p>Etat civil</p>
 		<input type="text" name="etat_civil" value={formData.etat_civil} onChange={handleChange} placeholder="Etat civil" />
 	  </div>
-	  <div className={style.field_container}>
-		<p className={style.field_name}>Quotient familial</p>
+	  <div className="auth-field-container">
+		<p>Quotient familial</p>
 		<input type="text" name="quotient_familial" value={formData.quotient_familial} onChange={handleChange} placeholder="Quotient familial" />
 	  </div>
-	  <div className={style.field_container}>
-		<p className={style.field_name}>Situation spécifique</p>
+	  <div className="auth-field-container">
+		<p>Situation spécifique</p>
 		<input type="text" name="situation_specifique" value={formData.situation_specifique} onChange={handleChange} placeholder="Situation spécifique" />
 	  </div>
-	  <div className={style.field_container}>
-		<p className={style.field_name}>RNI</p>
+	  <div className="auth-field-container">
+		<p>RNI</p>
 		<input type="text" name="rni" value={formData.rni} onChange={handleChange} placeholder="Revenus nets imposables (par an)" />
 	  </div>
-	  <div className={style.field_container}>
-		<p className={style.field_name}>CSP</p>
+	  <div className="auth-field-container">
+		<p>CSP</p>
 		<input type="text" name="csp" value={formData.csp} onChange={handleChange} placeholder="Catégorie socio professionnelle" />
 	  </div>
 	  <div>
